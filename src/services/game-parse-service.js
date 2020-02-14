924ba4149b065b821fd4d7b21228d623057d4cbf
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2020-02-14 18:06:24 -0500

    Feature/track current page (#9)
    * Tracks the page the parser is on so it returns to that page on reboot.
    
    * Fixes call
    
    * Keeps it from restarting every time the HTML scraper errors.
    
    * Makes it run by default on restart.
    
    * Adds some more logging.
    
    * Fixes one log

--- src/services/game-parse-service.js
+++ src/services/game-parse-service.js
@@ -19,51 +19,69 @@ export class GameParseService {
         for (const s of sites) {
             if (!this.paused) {
                 console.log(`Parsing site with url: ${s.url} and link selector: ${s.linkSelector}`);
-                await this._parseSite(s);
+                await this._parseSite(s, s.currentPage || null);
             }
         }
     }
 
     async _parseSite(site, page = null) {
+        this.gameSourceService.updatePage(site.name, page);
+
         // Get all the links
         let siteUrl = encodeURIComponent(site.url);
         if (page != null) {
             siteUrl = encodeURIComponent(url.resolve(site.url, page));
         }
         const linkSelector = encodeURIComponent(site.linkSelector);
-        const response = await axios.get(`${this.scraperBaseUrl}scrape/link?url=${siteUrl}&selector=${linkSelector}`);
-        for (const a of response.data) {
-            console.log(`Found link: ${a.content} (${a.link})`);
-            let entity;
-            try {
-                await this.gameService.upsert(a.content, site.name, a.link);
-                entity = await this.gameService.find(a.content);
-            } catch (err) {
-                console.error(chalk.red(`Error updating DB record for ${a.content}`));
-            }
-
-            try {
-                if (entity) {
-                    const ch = await this.queue.connect();
-                    const hasQ = await ch.assertQueue(this.gamesQueueName);
-                    if (hasQ) {
-                        console.log(`Sending to queue: ${entity.name}`);
-                        ch.sendToQueue(this.gamesQueueName, Buffer.from(JSON.stringify(entity)));
-                    }
+        try {
+            const response = await axios.get(`${this.scraperBaseUrl}scrape/link?url=${siteUrl}&selector=${linkSelector}`);
+            for (const a of response.data) {
+                console.log(`Found link: ${a.content} (${a.link})`);
+                let entity;
+                try {
+                    await this.gameService.upsert(a.content, site.name, a.link);
+                    entity = await this.gameService.find(a.content);
+                } catch (err) {
+                    console.error(chalk.red(`Error updating DB record for ${a.content}`));
                 }
-            } catch (err) {
-                console.error(chalk.red(`Error sending to queue for ${entity.name}`));
+
+                try {
+                    if (entity) {
+                        const ch = await this.queue.connect();
+                        const hasQ = await ch.assertQueue(this.gamesQueueName);
+                        if (hasQ) {
+                            console.log(`Sending to queue: ${entity.name}`);
+                            ch.sendToQueue(this.gamesQueueName, Buffer.from(JSON.stringify(entity)));
+                        }
+                    }
+                } catch (err) {
+                    console.error(chalk.red(`Error sending to queue for ${entity.name}`));
+                }
             }
+        } catch (err) {
+            console.error(err);
+            setTimeout(() => this._parseSite(site, page), 5000);
         }
 
         // Get the next page link and parse if exists
         const nextPageSelector = encodeURIComponent(site.nextPageSelector);
-        const nextPageResponse = await axios.get(`${this.scraperBaseUrl}scrape/link?url=${siteUrl}&selector=${nextPageSelector}`);
-        if (nextPageResponse.data && !!nextPageResponse.data.length) {
-            if (!this.paused) {
-                console.log(`Navigating to ${nextPageResponse.data[0].link}`);
-                await this._parseSite(site, nextPageResponse.data[0].link);
+        try {
+            const nextPageResponse = await axios.get(`${this.scraperBaseUrl}scrape/link?url=${siteUrl}&selector=${nextPageSelector}`);
+            if (nextPageResponse.data && !!nextPageResponse.data.length) {
+                const nextPage = nextPageResponse.data[0].link;
+                if (this.paused) {
+                    console.log(`Poller for ${site.name} paused. Next run will start on ${nextPage}`);
+                    this.gameSourceService.updatePage(site.name, nextPage);
+                } else {
+                    console.log(`Poller for ${site.name} navigating to ${nextPage}`);
+                    await this._parseSite(site, nextPage);
+                }
+            } else {
+                this.gameSourceService.updatePage(site.name, null);
+                console.log(`Poller for ${site.name} complete!`);
             }
+        } catch (err) {
+            console.error(err);
         }
     }
 }
